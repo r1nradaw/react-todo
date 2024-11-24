b436e52f6fa71fbd665209f198b942f586d6e773
Rinrada Wattanachanasorn <rinradawattanachanasorn@Rinradas-MacBook-Air.local>
2024-11-25 01:26:47 +0700

    another final touch

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -12,8 +12,12 @@ export function TodoItem({
         todo.completed ? "line-through" : ""
       }`}
     >
-      <div className="flex gap-2 ">
-        <input onChange={() => handleChecked(todo.id)} type="checkbox"></input>
+      <div className="flex gap-2 items-center ">
+        <input
+          className="w-[16px] h-[16px]"
+          onChange={() => handleChecked(todo.id)}
+          type="checkbox"
+        ></input>
         <span className="font-semibold">{todo.title}</span>
       </div>
       <button
